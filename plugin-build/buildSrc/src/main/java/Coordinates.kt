98efd7a1f49ff875b18548a7b0c6ecccc584095e
ni <ni@kode.ru>
2022-06-20 14:25:12 +0200

    Increase version to 1.1.0-alpha07

--- plugin-build/buildSrc/src/main/java/Coordinates.kt
+++ plugin-build/buildSrc/src/main/java/Coordinates.kt
@@ -1,7 +1,7 @@
 object PluginCoordinates {
     const val ID = "ru.kode.android.build-publish"
     const val GROUP = "ru.kode.android"
-    const val VERSION = "1.1.0-alpha06"
+    const val VERSION = "1.1.0-alpha07"
     const val IMPLEMENTATION_CLASS = "ru.kode.android.build.publish.plugin.BuildPublishPlugin"
 }
 
