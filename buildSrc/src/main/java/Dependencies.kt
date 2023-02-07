7dd10c87ef0770b7b0838beae8034a17845cd5fb
Maxim Zherenkov <wellaje@gmail.com>
2023-02-07 17:03:08 +0700

    increase kotlin and gradle version

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -4,11 +4,11 @@ object Versions {
 
 object BuildPluginsVersion {
     const val DETEKT = "1.19.0"
-    const val KOTLIN = "1.6.21"
+    const val KOTLIN = "1.8.10"
     const val KTLINT = "10.0.0"
     const val VERSIONS_PLUGIN = "0.38.0"
     const val APP_DISTRIBUTION_PLUGIN = "3.0.0"
-    const val ANDROID_PLUGIN = "7.0.4"
+    const val ANDROID_PLUGIN = "7.4.1"
     const val GRGIT_PLUGIN = "5.0.0"
 }
 
