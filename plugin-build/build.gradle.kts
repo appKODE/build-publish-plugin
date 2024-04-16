c6561f6f9c133bb851ab365b73e10bb5647b3c89
rinekri <rinekri@gmail.com>
2024-04-16 20:51:06 +0200

    Increase version to 1.3.0-alpha03

--- plugin-build/build.gradle.kts
+++ plugin-build/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 allprojects {
     group = "ru.kode.android"
-    version = "1.3.0-alpha02"
+    version = "1.3.0-alpha03"
 }
 
 val dependsOnRecursivelyByName = { task: Task, name: String ->
