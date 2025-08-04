758c611fb36ed4bed61cfea82806c1e7f544a7ee
rinekri <rinekri@gmail.com>
2025-08-04 20:24:18 +0100

    Make some constants in core public

--- plugin-build/plugin-core/src/main/java/ru/kode/android/build/publish/plugin/core/util/DirectoryExtensions.kt
+++ plugin-build/plugin-core/src/main/java/ru/kode/android/build/publish/plugin/core/util/DirectoryExtensions.kt
@@ -4,7 +4,7 @@ import org.gradle.api.Project
 import org.gradle.api.file.RegularFile
 import org.gradle.api.provider.Provider
 
-internal const val CHANGELOG_FILENAME = "changelog.txt"
+const val CHANGELOG_FILENAME = "changelog.txt"
 
 fun Project.changelogDirectory(): Provider<RegularFile> {
     return this.layout.buildDirectory.file(CHANGELOG_FILENAME)
